75d8d5f1b0f1444f6ed5565980ef4d8332babbf6
Lolliesoft <starplush@gmail.com>
2020-07-31 16:27:11 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Models.Restaurant {
+                Id = 1, Name = "Amir's Pizza Place" 
+            };
+
+            //return new ObjectResult(model); //creates a json endpoint
+
+            return View(model); //returns a view result
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configuration, IGreeter greeter, ILogger<Startup> logger)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            
+            app.UseRouting();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //forwards request to a default controller
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoutes);
+
+            app.Run(async (context) =>
+                {
+                    var greeting = greeter.GetMessageOfTheDay();
+                    context.Response.ContentType = "text/plain";
+                    //await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName}");
+                    await context.Response.WriteAsync($"Not Found");
+                });
+            
+            
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        //var greeting = configuration["Greeting"];
+
+            //        var greeting = greeter.GetMessageOfTheDay();
+            //        await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName}");
+            //    });
+            //});
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            // /Home/Index  Controllername/ActionName
+
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
